583228b117e364179791a26ffe43221e86d8c5ee
ezequielcarlos <ezequielcarlos@gmail.com>
2019-10-10 07:04:54 -0300

    adicao de um usuario ao bd

--- Site.py
+++ Site.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drogas.db'
 db = SQLAlchemy(app)
 
+class usuario(db.Model):
+    id = db.Colun(db.Integer, primary_key = True)
+    username = db.Colun(db.String(80), unique=True, nullable=False)
+    email = db.Colun(db.String(200), unique=True, nullable=False)
+    password = db.Colun(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return"<User %r>" % self.username
+
 @app.route('/')
 def index():
     return render_template('index.html')
