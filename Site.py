a93340ff02fdf1a2542f55131840578e2b1c65cb
ezequielcarlos <ezequielcarlos@gmail.com>
2019-10-10 06:48:51 -0300

    adicao do sqlalchemy

--- Site.py
+++ Site.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from flask-sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drogas.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
