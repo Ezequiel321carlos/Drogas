534e1e55f34b58bee8fbb9cac8bd286c8486027d
Ezequiel321carlos <ezequielcarlos119@gmail.com>
2019-11-07 09:59:39 -0300

    implementação do bd

--- Site.py
+++ Site.py
@@ -1,20 +1,10 @@
 from flask import Flask, render_template
-#from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drogas.db'
-#db = SQLAlchemy(app)
-
-#class usuario(db.Model):
-    #id = db.Column(db.Integer, primary_key = True)
-    #username = db.Column(db.String(80), unique=True, nullable=False)
-    #email = db.Column(db.String(200), unique=True, nullable=False)
-    #password = db.Column(db.String(20), nullable=False)
-
-    #def __repr__(self):
-        #return"<User %r>" % self.username
 
 @app.route('/')
+@app.route('/a')
 def index():
     return render_template('index.html')
 
@@ -24,6 +14,7 @@ def login():
     return render_template('login.html')
 
 @app.route('/cadastro')
+@app.route('/c')
 def cadastro():
     return render_template('cadastro.html')
 
