ac24e3e5419aeda47e553285a01438e554120297
Ezequiel321carlos <ezequielcarlos119@gmail.com>
2019-12-05 09:19:45 -0300

    alteração no BD

--- BD.py
+++ BD.py
@@ -6,9 +6,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drogas.db'
 
 class usuario(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    nome = db.Column(db.String(80), unique=True, nullable=False)
+    sobrename = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
-
+    CPF = db.Column(db.String(11), unique=True, nullable=False)
+    RG = db.Column(db.String(10), unique=True, nullable=False)
+    telefone = db.Column(db.String(8), unique=True, nullable=False)
+    estcv = db.Column(db.String(15), unique=True, nullable=False)
     def __repr__(self):
         return'<<User: %r>>' % self.username
